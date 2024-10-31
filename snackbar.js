76edc667d931afbf4ede74bdafb923ca8f5a8efe
YevheniiaZalevska <150593064+YevheniiaZalevska@users.noreply.github.com>
2024-10-31 12:55:27 +0000

    Deploying to gh-pages from @ YevheniiaZalevska/goit-js-hw-10@c52cf2a4ec8d7c2e237b6d5821df501440cf14b3 🚀

---
+++ snackbar.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{i as r}from"./assets/vendor-BbSUbo7J.js";document.querySelector(".form").addEventListener("submit",function(t){t.preventDefault();const s=Number(t.target.elements.delay.value),i=t.target.elements.state.value;new Promise((e,m)=>{setTimeout(()=>{i==="fulfilled"?e(s):m(s)},s)}).then(e=>{r.success({title:"✅ Success",message:`Fulfilled promise in ${e}ms`})}).catch(e=>{r.error({title:"❌ Error",message:`Rejected promise in ${e}ms`})})});
+//# sourceMappingURL=snackbar.js.map
